eb559c189cd2444f04e65b1bfe72d39394c8eb67
Yue Zhao <yuezhao@cs.toronto.edu>
2019-01-31 15:40:00 -0500

    Add script for conference trending (wip)

---
+++ plot.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""A plotting utility for conference trend
+"""
+# Author: Yue Zhao <yuezhao@cs.toronto.edu>
+# License: BSD 2 clause
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Todo
